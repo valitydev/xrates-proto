08fd47f4565088bdeed4b283d57624d3156a96c5
Pavel Popov <tolkonepiu@users.noreply.github.com>
2018-11-21 16:23:55 +0300

    BJ-307: x-rates proto (#1)
    * x-rates proto
    
    * Review fixes
    
    * Remove statuses, finalyze proto
    
    * Remove SourceID from ExchangeRateData, add docs
    
    * Review fixes
    
    * exchangeRateData -> exchange_rate_data

---
+++ proto/base.thrift
@@ -0,0 +1,24 @@
+namespace java com.rbkmoney.xrates.base
+namespace erlang base
+
+/**
+ * Отметка во времени согласно RFC 3339.
+ *
+ * Строка должна содержать дату и время в UTC в следующем формате:
+ * `2016-03-22T06:12:27Z`.
+ */
+typedef string Timestamp
+
+/**
+ * Временной интервал
+ */
+struct TimestampInterval {
+    1: required Timestamp lower_bound_inclusive
+    2: required Timestamp upper_bound_exclusive
+}
+
+/** Рациональное число. */
+struct Rational {
+    1: required i64 p
+    2: required i64 q
+}
---
+++ proto/rate.thrift
@@ -0,0 +1,82 @@
+namespace java com.rbkmoney.xrates.rate
+namespace erlang rate
+
+include "base.thrift"
+
+typedef i64 EventID
+typedef base.Rational ExchangeRate
+
+/** ISO 4217 */
+typedef string CurrencySymbolicCode
+
+typedef string SourceID
+
+struct EventRange {
+    1: optional EventID after
+    2: required i32 limit
+}
+
+exception NoLastEvent {}
+
+struct Currency {
+    1: required CurrencySymbolicCode symbolic_code
+    2: required i16 exponent
+}
+
+union Change {
+    1: ExchangeRateCreated created
+}
+
+struct ExchangeRateCreated {
+    1: required ExchangeRateData exchange_rate_data
+}
+
+/**
+*  Информация по курсам валют, где:
+*  interval - интервал времени в рамках которого действуют заданные курсы
+*  quotes - список курсов валют
+*/
+struct ExchangeRateData {
+    1: required base.TimestampInterval interval
+    2: required list<Quote> quotes
+}
+
+/**
+* Курс валют, где:
+* source - валюта, из которой конвертируют
+* destination - валюта, в которую конвертируют
+* exchange_rate - рациональное число конверсии в учете минорных единиц
+*
+* Пример:
+* Предположим, что мы конвертируем CLF в RUB по курсу 2640.4546 рублей.
+* В итоге source валюта здесь это CLF с экспонентой 4, destination валюта это RUB с экспонентой 2.
+* Получается, что одна минорная единица CLF равна 0.26404546 рубля, или 26.404546 копеек (минорная единица рубля).
+* В результате в рациональном представлении это будет равно "26404546 / 1000000" или "13202273 / 500000".
+*
+*/
+struct Quote {
+    1: required Currency source
+    2: required Currency destination
+    3: required ExchangeRate exchange_rate
+}
+
+struct Event {
+    1: required list<Change> changes
+}
+
+struct SinkEvent {
+    1: required EventID        id
+    2: required base.Timestamp created_at
+    3: required SourceID       source
+    4: required Event          payload
+}
+
+service EventSink {
+
+    list<SinkEvent> GetEvents (1: EventRange range)
+        throws ()
+
+    EventID GetLastEventID ()
+        throws (1: NoLastEvent ex1)
+
+}
